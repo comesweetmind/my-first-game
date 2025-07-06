ff17cee7e48ec81aba08f402b87011a3ac1c9679
comesweetmind <comesweetmind@gmail.com>
2025-07-07 00:52:40 +0300

    end 4

--- Player/player.gd
+++ Player/player.gd
@@ -1,25 +1,59 @@
+# player.gd (на CharacterBody2D)
 extends CharacterBody2D
 
+@onready var animated = $AnimatedSprite2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+# швидкість руху
+const SPEED = 200
+# сила стрибка
+const JUMP_VELOCITY = -400
+# гравітація
+const GRAVITY = 1200
 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func _physics_process(delta):
+	# Горизонтальний рух (ліва/права)
+	var direction = 0
+	var state = ""
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	
+	if Input.is_action_pressed("move_left"):
+		direction -= 1
+	if Input.is_action_pressed("move_right"):
+		direction += 1
+
+	
+	
+	if direction < 0:
+		animated.flip_h = true
+	elif direction > 0:
+		animated.flip_h = false
+	
+	velocity.x = direction * SPEED
+	
+	
+	# Стрибок
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = JUMP_VELOCITY
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		# Додаємо гравітацію
+		velocity.y += GRAVITY * delta
+	
+	# Вибір анімації
+	if not is_on_floor():
+		state = "Jump"
+	elif direction != 0:
+		state = "Run"
+	else:
+		state = "Idle"
+		
+	# Програвання анімації лише при зміні
+	if animated.animation != state:
+		animated.play(state)
+	
+	print(state)
+	# Застосовуємо рух
+	self.velocity = velocity
 	move_and_slide()
